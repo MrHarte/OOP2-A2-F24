b5b79290ef1a9ff1984adb9d50fac4a4469eaf80
SMireault <le0636183@crc-lennox.qc.ca>
2024-09-12 21:02:30 -0400

    Change aCards to a interface,
    initialize the aCards in the constructor,
    Add the sort method to Deck,
    With all the java doc needed.

--- src/main/java/com/champlain/oop2assignment2/Deck.java
+++ src/main/java/com/champlain/oop2assignment2/Deck.java
@@ -4,10 +4,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Deck implements CardSource {
-    private final List<Card> aCards = new ArrayList<>();
 
+public class Deck implements CardSource {
+    /**
+     * This is a final list that holds all the {@link Card} objects in the {@code Deck}.
+     */
+    private final List<Card> aCards;
+
+    /**
+     * It iterates over all possible {@link Rank} and {@link Suit} combinations and
+     * adds each card to the {@code aCards} list.
+     */
     public Deck() {
+        aCards = new ArrayList<>();
         for (Rank currentRank : Rank.values()) {
             for (Suit currentSuit : Suit.values()) {
                 this.aCards.add(new Card(currentRank, currentSuit));
@@ -19,8 +28,15 @@ public class Deck implements CardSource {
         Collections.shuffle(this.aCards);
     }
 
+    /**
+     * Sorts the deck of cards. The sorted order prioritizes suits and ranks.
+     */
+    public void sort() {
+        Collections.sort(this.aCards);
+    }
+
     public Card draw() {
-        int last = this.aCards.size()-1;
+        int last = this.aCards.size() - 1;
         Card myCard = this.aCards.get(last);
         this.aCards.remove(last);
         return myCard;
