b96eab28553c7495388a27598aab2a21844f23ee
Joey Knowles <Joey.Knowles@stu.crc-lennox.qc.ca>
2024-09-19 18:03:49 -0400

    Added more java.doc to the classes.

--- src/main/java/com/champlain/oop2assignment2/Deck.java
+++ src/main/java/com/champlain/oop2assignment2/Deck.java
@@ -31,6 +31,16 @@ public class Deck implements CardSource, Iterable<Card> {
         Collections.shuffle(this.aCards);
     }
 
+    /**
+     * Sorts the deck first by suit in order from:
+     * Clubs, Diamonds, Spades, Hearts. Then, for cards of the same
+     * suit, sorts them by rank in order from: Ace, Two, Three, ...,
+     * Jack, Queen, King.
+     * <p>
+     * This method uses the {@link java.util.Collections#sort(List)} method to
+     * sort the deck.
+     * </p>
+     */
     public void sort() {
         Collections.sort(this.aCards);
     }
@@ -46,6 +56,16 @@ public class Deck implements CardSource, Iterable<Card> {
         return this.aCards.isEmpty();
     }
 
+
+    /**
+     * Returns an iterator over the cards in the deck.
+     * <p>
+     * The iteration order follows the current state of the deck, if the
+     * deck is shuffled or sorted, the iterator will follow that order.
+     * </p>
+     *
+     * @return An iterator for iterating over the cards in the deck.
+     */
     @Override
     public Iterator<Card> iterator() {
         return this.aCards.iterator();
--- src/main/java/com/champlain/oop2assignment2/DeckController.java
+++ src/main/java/com/champlain/oop2assignment2/DeckController.java
@@ -20,12 +20,27 @@ public class DeckController {
         this.displayDeck();
     }
 
+    /**
+     * Handles the click event for the "Sort" button.
+     *<p>
+     * When clicked, this method sorts the deck of cards by suit and rank,
+     * then it displays the sorted deck.
+     *</p>
+     */
     @FXML
     protected void onSortButtonClick() {
         this.aDeck.sort();
         this.displayDeck();
     }
 
+    /**
+     * Handles the click event for the "Show" button.
+     * <p>
+     * When clicked, this method steps through the deck and displays
+     * a confirmation alert for every card in the deck, showing
+     * its details.
+     * </p>
+     */
     @FXML
     protected void onShowButtonClick() {
         for (Card card : this.aDeck) {
@@ -37,6 +52,15 @@ public class DeckController {
         this.aDeckTextArea.setText(this.aDeck.toString());
     }
 
+    /**
+     * Shows a confirmation alert displaying the details of a given card.
+     * <p>
+     * This method is used when stepping through the deck, displaying
+     * each card as a confirmation alert.
+     * </p>
+     *
+     * @param card The card to display in the confirmation alert.
+     */
     private void showConfirmationAlert(Card card) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Confirmation");
