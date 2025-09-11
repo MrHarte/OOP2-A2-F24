4bde03fafb830bbe7322eed8bbed0532d0a6e54e
pfelix7 <perrefelix@gmail.com>
2025-09-11 15:41:26 -0400

    Implemented the Comparable interface in Card. Added sort method in Deck.

--- src/main/java/com/champlain/oop2assignment2/DeckController.java
+++ src/main/java/com/champlain/oop2assignment2/DeckController.java
@@ -21,7 +21,9 @@ public class DeckController {
 
     @FXML
     protected void onSortButtonClick() {
-        this.aDeckTextArea.setText("This does not sort anything yet.");
+        this.aDeck.sort();
+        this.displayDeck();
+        //this.aDeckTextArea.setText("This does not sort anything yet.");
     }
 
     @FXML
