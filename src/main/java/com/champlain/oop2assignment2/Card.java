06ff37c802e373157e87ff5975af5548d747a152
CodeDexx <ianraharolahy@gmail.com>
2025-09-11 15:35:01 -0400

    This Is Commit

--- src/main/java/com/champlain/oop2assignment2/Card.java
+++ src/main/java/com/champlain/oop2assignment2/Card.java
@@ -1,25 +1,41 @@
 package com.champlain.oop2assignment2;
 
-public class Card {
-    private final Suit aSuit;
+public class Card implements Comparable<Card> {
+    private String suit;
+    private String rank;
 
-    private final Rank aRank;
-
-    public Card (Rank pRank, Suit pSuit) {
-        this.aRank = pRank;
-        this.aSuit = pSuit;
+    public Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
     }
 
-    public Rank getRank() {
-        return this.aRank;
+    public String getSuit() {
+        return suit;
     }
 
-    public Suit getSuit() {
-        return this.aSuit;
+    public String getRank() {
+        return rank;
+    }
+
+    @Override
+    public int compareTo(Card other) {
+        String[] suitOrder = { "Clubs", "Diamonds", "Hearts", "Spades" };
+        int thisSuit = java.util.Arrays.asList(suitOrder).indexOf(this.suit);
+        int otherSuit = java.util.Arrays.asList(suitOrder).indexOf(other.suit);
+
+        if (thisSuit != otherSuit) {
+            return Integer.compare(thisSuit, otherSuit);
+        }
+
+        String[] rankOrder = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+        int thisRank = java.util.Arrays.asList(rankOrder).indexOf(this.rank);
+        int otherRank = java.util.Arrays.asList(rankOrder).indexOf(other.rank);
+
+        return Integer.compare(thisRank, otherRank);
     }
 
     @Override
     public String toString() {
-        return this.aRank + " of " + this.aSuit;
+        return rank + " of " + suit;
     }
 }
